fe2bd015031349e0e80d474c263fec992eca2444
Rob Loach <robloach@gmail.com>
2022-12-18 23:04:06 -0500

    Add LoadBMP

--- examples/SDL_physfs_example.c
+++ examples/SDL_physfs_example.c
@@ -4,57 +4,63 @@
 #include "SDL_physfs.h"
 
 int main() {
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		fprintf(stderr, "SDL_Init Error: %s\n", SDL_GetError());
-		return EXIT_FAILURE;
-	}
+	SDL_Init(SDL_INIT_EVERYTHING);
 
-	SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 620, 387, SDL_WINDOW_SHOWN);
-	if (win == NULL) {
-		fprintf(stderr, "SDL_CreateWindow Error: %s\n", SDL_GetError());
-		return EXIT_FAILURE;
-	}
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+	SDL_Window* window = SDL_CreateWindow("SDL_physfs: Example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 450, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
-	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (ren == NULL) {
-		fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
-		SDL_DestroyWindow(win);
-		SDL_Quit();
-		return EXIT_FAILURE;
-	}
+    // Initialize PhysFS.
+    SDL_PhysFS_Init();
 
-	SDL_Surface* bmp = SDL_LoadBMP("resources/test-image.bmp");
-	if (bmp == NULL) {
-		fprintf(stderr, "SDL_LoadBMP Error: %s\n", SDL_GetError());
-		SDL_DestroyRenderer(ren);
-		SDL_DestroyWindow(win);
-		SDL_Quit();
-		return EXIT_FAILURE;
-	}
+    // Mount
+    SDL_PhysFS_Mount("resources", "res");
 
-	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
-	if (tex == NULL) {
-		fprintf(stderr, "SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
-		SDL_FreeSurface(bmp);
-		SDL_DestroyRenderer(ren);
-		SDL_DestroyWindow(win);
-		SDL_Quit();
-		return EXIT_FAILURE;
-	}
+    // Load the BMP from the SDL_RWops.
+    SDL_Surface* bmp = SDL_PhysFS_LoadBMP("res/test-image.bmp");
+
+    // Create a texture from the surface.
+	SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, bmp);
+
+    const int textureWidth = bmp->w;
+    const int textureHeight = bmp->h;
 	SDL_FreeSurface(bmp);
 
-	for (int i = 0; i < 20; i++) {
-		SDL_RenderClear(ren);
-		SDL_RenderCopy(ren, tex, NULL, NULL);
-		SDL_RenderPresent(ren);
-		SDL_Delay(100);
-	}
+    int quit = 0;
+    SDL_Event event;
 
+    while (quit == 0) {
+        while (SDL_PollEvent(&event) != 0) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    quit = 1;
+                    break;
+                case SDL_KEYDOWN:
+                    if (event.key.keysym.sym == SDLK_ESCAPE) {
+                        quit = 1;
+                    }
+                    break;
+            }
+        }
+
+        SDL_Rect destination = {
+            screenWidth / 2 - textureWidth / 2,
+            screenHeight / 2 - textureHeight / 2,
+            textureWidth,
+            textureHeight
+        };
+
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, tex, NULL, &destination);
+		SDL_RenderPresent(renderer);
+    }
+
+    SDL_PhysFS_Quit();
 	SDL_DestroyTexture(tex);
-	SDL_DestroyRenderer(ren);
-	SDL_DestroyWindow(win);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
-
 
     return 0;
 }
--- include/SDL_physfs.h
+++ include/SDL_physfs.h
@@ -9,6 +9,13 @@ extern "C" {
 #include "physfs.h"
 
 // Declarations
+int SDL_PhysFS_Init();
+int SDL_PhysFS_Quit();
+int SDL_PhysFS_Mount(const char* newDir, const char* mountPoint);
+int SDL_PhysFS_MountFromMemory(const unsigned char *fileData, int dataSize, const char* newDir, const char* mountPoint);
+int SDL_PhysFS_Unmount(const char* oldDir);
+SDL_RWops* SDL_PhysFS_RWFromFile(const char* filename);
+SDL_Surface* SDL_PhysFS_LoadBMP(const char* filename);
 
 #ifdef __cplusplus
 }
@@ -25,6 +32,226 @@ extern "C" {
 #endif
 
 // Definitions
+
+int SDL_PhysFS_SetLastError(const char* functionName) {
+    int error = PHYSFS_getLastErrorCode();
+    SDL_SetError("%s: %s", functionName, PHYSFS_getErrorByCode(error));
+    return error;
+}
+
+/**
+ * Initialize the PhysFS virtual file system.
+ *
+ * @return 0 on success, non-zero otherwise.
+ *
+ * @see SDL_PhysFS_Quit()
+ */
+int SDL_PhysFS_Init() {
+    if (PHYSFS_init(0) == 0) {
+        return SDL_PhysFS_SetLastError("SDL_PhysFS_Init");
+    }
+
+    return PHYSFS_ERR_OK;
+}
+
+/**
+ * Close the PhysFS virtual file system.
+ *
+ * @return 0 on success, non-zero otherwise.
+ */
+int SDL_PhysFS_Quit() {
+    if (PHYSFS_deinit() == 0) {
+        return SDL_PhysFS_SetLastError("SDL_PhysFS_Quit");
+    }
+
+    return PHYSFS_ERR_OK;
+}
+
+/**
+ * Mounts the given directory, at the given mount point.
+ *
+ * @param newDir Directory or archive to add to the path, in platform-dependent notation.
+ * @param mountPoint Location in the interpolated tree that this archive will be "mounted", in platform-independent notation. NULL or "" is equivalent to "/".
+ *
+ * @return 0 on success, non-zero on failure.
+ *
+ * @see SDL_PhysFS_Unmount()
+ */
+int SDL_PhysFS_Mount(const char* newDir, const char* mountPoint) {
+    if (PHYSFS_mount(newDir, mountPoint, 1) == 0) {
+        return SDL_PhysFS_SetLastError("SDL_PhysFS_Mount");
+    }
+
+    return PHYSFS_ERR_OK;
+}
+
+/**
+ * Mounts the given file data as a mount point in PhysFS.
+ *
+ * @param fileData The archive data as a file buffer.
+ * @param dataSize The size of the file buffer.
+ * @param newDir A filename that can represent the file data. Has to be unique. For example: data.zip
+ * @param mountPoint The location in the tree that the archive will be mounted.
+ *
+ * @return 0 on success, non-zero on failure.
+ *
+ * @see SDL_PhysFS_Mount()
+ */
+int SDL_PhysFS_MountFromMemory(const unsigned char *fileData, int dataSize, const char* newDir, const char* mountPoint) {
+    if (dataSize <= 0) {
+        return SDL_SetError("SDL_PhysFS_MountFromMemory: Cannot mount a data size of 0");
+    }
+
+    if (PHYSFS_mountMemory(fileData, dataSize, 0, newDir, mountPoint, 1) == 0) {
+        return SDL_PhysFS_SetLastError("SDL_PhysFS_MountFromMemory");
+    }
+
+    return PHYSFS_ERR_OK;
+}
+
+/**
+ * Unmounts the given directory or archive.
+ *
+ * @param oldDir The directory that was supplied to MountPhysFS's newDir.
+ *
+ * @return 0 on success, non-zero on failure.
+ *
+ * @see MountPhysFS()
+ */
+int SDL_PhysFS_Unmount(const char* oldDir) {
+    if (PHYSFS_unmount(oldDir) == 0) {
+        return SDL_PhysFS_SetLastError("SDL_PhysFS_Unmount");
+    }
+
+    return PHYSFS_ERR_OK;
+}
+
+Sint64 SDLCALL SDL_PhysFS_RWopsSize(struct SDL_RWops *rw) {
+    PHYSFS_File *handle = (PHYSFS_File *)rw->hidden.unknown.data1;
+    return (Sint64) PHYSFS_fileLength(handle);
+}
+
+Sint64 SDLCALL SDL_PhysFS_RWopsSeek(struct SDL_RWops *rw, Sint64 offset, int whence) {
+    PHYSFS_File *handle = (PHYSFS_File *)rw->hidden.unknown.data1;
+    PHYSFS_sint64 pos = 0;
+
+    if (whence == RW_SEEK_SET) {
+        pos = (PHYSFS_sint64) offset;
+    }
+    else if (whence == RW_SEEK_CUR) {
+        const PHYSFS_sint64 current = PHYSFS_tell(handle);
+        if (current == -1) {
+            return SDL_SetError("Can't find position in file: %s",
+                          PHYSFS_getErrorByCode(PHYSFS_getLastErrorCode()));
+        }
+
+        if (offset == 0) {
+            return (Sint64) current;
+        }
+
+        pos = current + ((PHYSFS_sint64) offset);
+    }
+    else if (whence == RW_SEEK_END) {
+        const PHYSFS_sint64 len = PHYSFS_fileLength(handle);
+        if (len == -1) {
+            return SDL_SetError("Can't find end of file: %s", PHYSFS_getErrorByCode(PHYSFS_getLastErrorCode()));
+        }
+
+        pos = len + ((PHYSFS_sint64) offset);
+    }
+    else {
+        return SDL_SetError("Invalid 'whence' parameter.");
+    }
+
+    if (pos < 0) {
+        return SDL_SetError("Attempt to seek past start of file.");
+    }
+    
+    if (!PHYSFS_seek(handle, (PHYSFS_uint64)pos)) {
+        return SDL_SetError("PhysicsFS error: %s", PHYSFS_getErrorByCode(PHYSFS_getLastErrorCode()));
+    }
+
+    return (Sint64) pos;
+}
+
+size_t SDL_PhysFS_RWopsRead(struct SDL_RWops *rw, void *ptr, size_t size, size_t maxnum) {
+    PHYSFS_File *handle = (PHYSFS_File *) rw->hidden.unknown.data1;
+    const PHYSFS_uint64 readlen = (PHYSFS_uint64) (maxnum * size);
+    const PHYSFS_sint64 rc = PHYSFS_readBytes(handle, ptr, readlen);
+    if (rc != ((PHYSFS_sint64) readlen)) {
+        if (!PHYSFS_eof(handle)){
+            return SDL_SetError("PhysicsFS error: %s", PHYSFS_getErrorByCode(PHYSFS_getLastErrorCode()));
+        }
+    } 
+
+    return (size_t) rc / size;
+}
+
+size_t SDLCALL SDL_PhysFS_RWopsWrite(struct SDL_RWops *rw, const void *ptr, size_t size, size_t num) {
+    PHYSFS_File *handle = (PHYSFS_File *) rw->hidden.unknown.data1;
+    const PHYSFS_uint64 writelen = (PHYSFS_uint64) (num * size);
+    const PHYSFS_sint64 rc = PHYSFS_writeBytes(handle, ptr, writelen);
+    if (rc != ((PHYSFS_sint64) writelen)) {
+        SDL_SetError("PhysicsFS error: %s", PHYSFS_getErrorByCode(PHYSFS_getLastErrorCode()));
+    }
+
+    return (size_t) rc;
+}
+
+static int SDL_PhysFS_RWopsClose(SDL_RWops *rw) {
+    if (rw == NULL) {
+        return 0;
+    }
+
+    PHYSFS_File *handle = (PHYSFS_File *)rw->hidden.unknown.data1;
+    if (handle != NULL) {
+        if (!PHYSFS_close(handle)) {
+            return SDL_SetError("PhysicsFS error: %s", PHYSFS_getErrorByCode(PHYSFS_getLastErrorCode()));
+        }
+
+        SDL_FreeRW(rw);
+    }
+
+    return 0;
+}
+
+SDL_RWops *SDL_PhysFS_RWopsCreate(PHYSFS_File *handle) {
+    SDL_RWops *retval = NULL;
+
+    if (handle == NULL) {
+        SDL_SetError("PHYSFS: %s", PHYSFS_getErrorByCode(PHYSFS_getLastErrorCode()));
+    }
+    else {
+        retval = SDL_AllocRW();
+        if (retval != NULL) {
+            retval->size  = SDL_PhysFS_RWopsSize;
+            retval->seek  = SDL_PhysFS_RWopsSeek;
+            retval->read  = SDL_PhysFS_RWopsRead;
+            retval->write = SDL_PhysFS_RWopsWrite;
+            retval->close = SDL_PhysFS_RWopsClose;
+            retval->hidden.unknown.data1 = handle;
+        }
+    }
+
+    return retval;
+}
+
+SDL_RWops* SDL_PhysFS_RWFromFile(const char* filename) {
+    PHYSFS_File* handle = PHYSFS_openRead(filename);
+    if (handle == NULL) {
+        SDL_PhysFS_SetLastError("SDL_PhysFS_LoadRWops");
+        return NULL;
+    }
+    return SDL_PhysFS_RWopsCreate(handle);
+}
+
+SDL_Surface* SDL_PhysFS_LoadBMP(const char* filename) {
+    SDL_RWops* rwops = SDL_PhysFS_RWFromFile(filename);
+    if (rwops == NULL) {
+        return NULL;
+    }
+    return SDL_LoadBMP_RW(rwops, 1);
+}
 
 #ifdef __cplusplus
 }
